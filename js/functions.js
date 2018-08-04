da84d7c19020f114873af26799cc159b6ca83edd
Marvin Jay <marventex@gmail.com>
2018-08-05 01:20:04 +0800

    contacts added

--- js/functions.js
+++ js/functions.js
@@ -17,6 +17,10 @@ $(window).scroll(function(){
       'transform' : 'translate(0px, '+ wScroll /5 +'%)'
     });
 
+  $('.back-guitar').css({
+    'transform' : 'translate(0px, '+ wScroll /8 +'%)'
+  });
+
   }
 
   });
@@ -69,3 +73,30 @@ $('.horizontal .progress-fill span').each(function(){
   var percent = $(this).html();
   $(this).parent().css('width', percent);
 });
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
